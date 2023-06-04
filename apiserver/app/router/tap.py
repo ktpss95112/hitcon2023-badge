66e0d854752beed61b2ab6c44706e963abf0981a
Chi-Feng Tsai <ktpss95112@gmail.com>
2023-06-04 17:18:50 +0800

    add endpoint for flush emoji

--- apiserver/app/router/tap.py
+++ apiserver/app/router/tap.py
@@ -18,6 +18,7 @@ def user_add_record(func):
         user.add_record(datetime.now(), reader)
         await db.write_user(user)
         return ret
+
     return wrapper
 
 
@@ -36,4 +37,17 @@ async def tap_sponsor(user: GetUserDep, reader: GetReaderDep, db: DBDep) -> bool
 )
 @user_add_record
 async def tap_popcat(user: GetUserDep, reader: GetReaderDep, db: DBDep) -> bool:
+    # TODO
+    return True
+
+
+@router.post(
+    "/sponsor_flush_emoji/{reader_id}/user/{card_uid}",
+    dependencies=[CheckCardReaderTypeDep(CardReaderType.SPONSOR_FLUSH)],
+)
+@user_add_record
+async def tap_sponsor_flush_emoji(
+    user: GetUserDep, reader: GetReaderDep, db: DBDep
+) -> bool:
+    # TODO
     return True
