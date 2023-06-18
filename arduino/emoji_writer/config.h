9e5980bd0cde43c0f0ac38f1492d92f74ab96f4f
aoaaceai <aoaaceai@csie.ntu.edu.tw>
2023-06-18 19:10:55 +0800

    set client certificate during initialization

--- arduino/emoji_writer/config.h
+++ arduino/emoji_writer/config.h
@@ -8,4 +8,6 @@
 #define GAME_HOST "www.redacted.com"
 #define GAME_HOST_PORT 443
 #define GAME_HOST_FINGERPRINT "87:87:87:87:87:87:87:87:87:87:87:87:87:87:87:87:87:87:87:87"
+#define GAME_CLIENT_CERT "-----BEGIN CERTIFICATE-----\n...-----END CERTIFICATE-----\n"
+#define GAME_CLIENT_KEY "-----BEGIN PRIVATE KEY-----\n...-----END PRIVATE KEY-----\n"
 #endif
