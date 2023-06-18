558de6b246b2f1e40f01bfe5251c92f70b96e325
Alx-Lai <b09902135@csie.ntu.edu.tw>
2023-06-18 18:19:02 +0800

    add simple python connect gui

---
+++ arduino/dino_run/dino_run.ino
@@ -0,0 +1,15 @@
+#include "card.h"
+
+/*
+ * Entrypoints
+ */
+void setup() {
+	Serial.begin(9600);
+	card::setup();
+}
+
+void loop() {
+	delay(100);
+	if (!card::legal_new_card())
+		return;
+	Serial.println("1");
