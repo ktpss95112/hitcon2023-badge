2b1c87c4ce27edcb9b5092778094c8e323a15d9e
Chi-Feng Tsai <ktpss95112@gmail.com>
2023-07-04 12:35:51 +0800

    fix project structure

---
+++ apiserver/app/main.py
@@ -0,0 +1,34 @@
+from contextlib import asynccontextmanager
+from datetime import datetime
+
+from fastapi import FastAPI
+
+from .db import init_db
+from .router import cardreader, popcat, tap, user
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    Create DB before the app starts.
+    """
+    init_db()
+    yield
+    # TODO: cleanup db
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(user.router)
+app.include_router(cardreader.router)
+app.include_router(tap.router)
+app.include_router(popcat.router)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/time")
+async def get_time() -> datetime:
+    return datetime.now()
