89d1f2aef176ce18c6833caea8c436d911c34415
aoaaceai <aoaaceai@csie.ntu.edu.tw>
2023-07-09 16:04:34 +0800

    Add some blank lines for readability

--- arduino/emoji_writer/game.h
+++ arduino/emoji_writer/game.h
@@ -18,10 +18,13 @@ namespace game {
 	const char *host = GAME_HOST;
 	const uint16_t host_port = GAME_HOST_PORT;
 	const char *host_fingerprint = GAME_HOST_FINGERPRINT;
+
 	const char *client_cert_str = GAME_CLIENT_CERT;
 	const char *client_key_str = GAME_CLIENT_KEY;
+
 	const char *emoji_timetable_path = "/cardreader/emoji_time_table/" GAME_READER_ID;
 	const char *current_time_path = "/time";
+
 	BearSSL::WiFiClientSecure wifi_client;
 	const BearSSL::X509List client_cert(client_cert_str);
 	const BearSSL::PrivateKey client_key(client_key_str);
