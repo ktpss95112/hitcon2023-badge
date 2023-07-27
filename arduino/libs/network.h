e4d13aa564cfa396145bfadeda1df4d150c7ed13
aoaaceai <aoaaceai@csie.ntu.edu.tw>
2023-07-28 00:28:36 +0800

    Add debug logs for emoji writer

--- arduino/libs/network.h
+++ arduino/libs/network.h
@@ -3,6 +3,7 @@
 
 #include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
+#include <ESP8266HTTPClient.h>
 #include "config.h"
 
 namespace network {
@@ -22,6 +23,7 @@ namespace network {
 	const BearSSL::PrivateKey client_key(client_key_str);
 
 	void setup();
+	String get_string(const char *path);
 	bool get_json(DynamicJsonDocument &doc, const char *path);
 	bool post_json(DynamicJsonDocument &doc, const char *path);
 };
