86f868aa86086a2279ceaa3cd1ee62baff40ae62
aoaaceai <aoaaceai@csie.ntu.edu.tw>
2023-07-29 19:46:49 +0800

    rename files and symbols to remove the usage of uuid

--- arduino/libs/card.h
+++ arduino/libs/card.h
@@ -8,7 +8,7 @@ namespace card {
 	const auto RST_PIN = D9;
 	const auto SS_PIN = D10;
 	const int BLKSIZE = 16;
-	const int UUIDSIZE = 4;
+	const int UIDSIZE = 4;
 
 	MFRC522 mfrc522(SS_PIN, RST_PIN);
 	MFRC522::MIFARE_Key default_key;
@@ -16,8 +16,8 @@ namespace card {
 	bool legal_new_card();
 	int pread(byte *buf, int nbyte, int offset);
 	int pwrite(byte *buf, int nbyte, int offset);
-	bool read_uuid(byte *buf);
-	bool write_uuid(byte *buf);
+	bool read_uid(byte *buf);
+	bool write_uid(byte *buf);
 	void done();
 }
 
