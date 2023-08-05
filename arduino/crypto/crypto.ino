9d6977c594040861b3fd73a2963fc98c064033e1
aoaaceai <aoaaceai@csie.ntu.edu.tw>
2023-08-05 21:35:41 +0800

    lcd support for crypto game

--- arduino/crypto/crypto.ino
+++ arduino/crypto/crypto.ino
@@ -1,14 +1,18 @@
 #include "game.h"
 #include "card.h"
+#include "lcd.h"
 
 void setup() {
 	Serial.begin(9600);
+	lcd::setup();
 	card::setup();
 }
 
 void loop() {
+	lcd::print_multi("crypto game\nscan card");
 	if (!card::legal_new_card())
 		return;
 	game::process_card();
 	card::done();
+	lcd::clear();
 }
--- arduino/libs/lcd.ino
+++ arduino/libs/lcd.ino
@@ -22,7 +22,7 @@ namespace lcd {
 		return row >= 0 && row < NROW;
 	}
 
-	static inline valid_col(int col) {
+	static inline bool valid_col(int col) {
 		return col >= 0 && col < NCOL;
 	}
 
@@ -36,7 +36,7 @@ namespace lcd {
 		if (len > NCOL)
 			return false;
 		
-		lcd.setCursor(row, 0);
+		lcd.setCursor(0, row);
 		lcd.print(msg);
 		return true;
 	}
@@ -51,9 +51,58 @@ namespace lcd {
 		if (len > NCOL)
 			return false;
 
-		lcd.setCursor(row, 0);
+		lcd.setCursor(0, row);
 		lcd.print(msg);
 
+		return true;
+	}
+
+	bool print(int row, const String &msg, int duration) {
+		if (!print(row, msg))
+			return false;
+		delay(duration);
+		clear(row);
+		return true;
+	}
+
+	bool print(int row, const char *msg, int duration) {
+		if (!print(row, msg))
+			return false;
+		delay(duration);
+		clear(row);
+		return true;
+	}
+
+	bool print_multi(const char *msg) {
+		String line;
+		const int n = strlen(msg);
+
+		for (int i = 0; i < n; ++i) {
+			if (msg[i] == '\n')
+				break;
+			line += msg[i];
+		}
+
+		const int n1 = line.length(), n2 = n - n1 - 1;
+
+		if (n1 == n)
+			return false;
+		if (n1 > NCOL)
+			return false;
+		if (n2 > NCOL)
+			return false;
+
+		print(0, line);
+		print(1, msg + line.length() + 1);
+
+		return true;
+	}
+
+	bool print_multi(const char *msg, int duration) {
+		if (!print_multi(msg))
+			return false;
+		delay(duration);
+		clear();
 		return true;
 	}
 
@@ -63,10 +112,16 @@ namespace lcd {
 		if (!valid_row(row))
 			return false;
 
-		lcd.setCursor(row, 0);
+		lcd.setCursor(0, row);
 		for (i = 0; i < NCOL; ++i)
 			lcd.print(' ');
 
+		return true;
+	}
+
+	bool clear() {
+		clear(0);
+		clear(1);
 		return true;
 	}
 
@@ -81,6 +136,7 @@ namespace lcd {
 		if (!valid_col(col))
 			return false;
 		
-		lcd.setCursor(row, col);
+		lcd.setCursor(col, row);
+		return true;
 	}
 }
