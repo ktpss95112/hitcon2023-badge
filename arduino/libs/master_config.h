8ba86216f6d52ebaecc239480577e473753a7fa6
Chi-Feng Tsai <ktpss95112@gmail.com>
2023-08-19 02:24:52 +0800

    update master_config.h

--- arduino/libs/master_config.h
+++ arduino/libs/master_config.h
@@ -1,6 +1,74 @@
+#define D1R2
+// #define D1R1
+
 #define WIFI_SSID "<redacted>"
 #define WIFI_PASSWD "<redacted>"
 #define WIFI_HOSTNAME "<redacted>"
+
+#define DEVCORE
+#define GAME_READER_ID "s1"
+#define WRITER
+
+// #define CYCRAFT
+// #define GAME_READER_ID "s2"
+// #define WRITER
+
+// #define FOXCONN
+// #define GAME_READER_ID "s3"
+// #define WRITER
+
+// #define TRAPA
+// #define GAME_READER_ID "s4"
+// #define WRITER
+
+// #define CHT_SEC
+// #define GAME_READER_ID "s5"
+// #define WRITER
+
+// #define RAKUTEN
+// #define GAME_READER_ID "s6"
+// #define WRITER
+
+// #define OFFSEC
+// #define GAME_READER_ID "s7"
+// #define WRITER
+
+// #define KKCOMPANY
+// #define GAME_READER_ID "s8"
+// #define WRITER
+
+// #define ISIP
+// #define GAME_READER_ID "s9"
+// #define WRITER
+
+// #define KLICKLACK
+// #define GAME_READER_ID "s10"
+// #define WRITER
+
+// #define GAME_READER_ID "sf1"
+// #define FLUSHER
+
+// #define RESET
+// #define GAME_READER_ID "c1"
+
+// #define ADD1
+// #define GAME_READER_ID "c2"
+
+// #define ROR
+// #define GAME_READER_ID "c3"
+
+// #define SWAP_HILO
+// #define GAME_READER_ID "c4"
+
+// #define RAND_FLIP
+// #define GAME_READER_ID "c5"
+
+// #define GAME_READER_ID "p1"
+
+// #define GAME_READER_ID "p2"
+
+#define RANDOM_SEED "<redacted>"
+#define CLOCK_SYNC_INTERVAL 100
 
 #define GAME_HOST "www.redacted.com"
 #define GAME_HOST_PORT 443
