5e18bcf67d8e849a2eeaa262c8523c5ae5ea78db
Alx-Lai <b09902135@csie.ntu.edu.tw>
2023-08-18 17:38:03 +0800

    fetch api while poke

--- arduino/crypto/crypto.h
+++ arduino/crypto/crypto.h
@@ -7,6 +7,8 @@ namespace crypto {
 	const int data_off = 272;
 	const int hmac_off = data_off + card::BLKSIZE;
 
+	const char *tap_record_path = "/tap/crypto/";
+
 	void setup();
 	bool process_card();
 }
